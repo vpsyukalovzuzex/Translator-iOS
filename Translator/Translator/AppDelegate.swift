cfd383b59463f57a78e27ffabed57d17131ecf06
Vladimir Psyukalov <vpsyukalov@zuzex.com>
2021-09-09 16:39:01 +0300

    Initial commit

---
+++ Translator/Translator/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+// AppDelegate.swift
+//
+
+import UIKit
+
+typealias LaunchOptions = [UIApplication.LaunchOptionsKey: Any]
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: LaunchOptions?) -> Bool {
+        return true
+    }
+}
